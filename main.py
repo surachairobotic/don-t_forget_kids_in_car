c903981d708233dac46b1043d19376d49d3b7a3e
Surachai Rodsai <surachairobotic@gmail.com>
2022-10-13 16:14:24 +0700

    ...

--- main.py
+++ main.py
@@ -1,11 +1,47 @@
-from fastapi import FastAPI
+import shutil, json, base64
+from fastapi import FastAPI, Request, File, UploadFile
 
 app = FastAPI()
+cnt=0
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+'''
 @app.post("/upload")
-def upload():
+async def upload(image: UploadFile = File(...)):
+    global cnt
+    print(cnt)
+    cnt+=1
+    
+    print(type(image))
+    print(image)
+    
+    with open("destination.png", "wb") as buffer:
+        shutil.copyfileobj(image.file, buffer)
+    
+    return {"filename": image.filename}
     return {"upload": "image"}
+
+'''
+
+@app.post("/upload")
+async def upload(req: Request): # var_name: var_type
+    global cnt
+    #content = '%s %s' % (req.method, req.url.path)
+    #print(content)
+    #print('%s, %s, %s' % (req.url.path, req.url.port, req.url.scheme))
+    req_info = await req.json()
+    str_image = req_info['image']
+    #print(type(str_image))
+    #binary_image = req_info['image'].encode('ascii')
+    #print(type(binary_image))
+    
+    fh = open("img_{}.png".format(cnt), "wb")
+    fh.write(base64.b64decode(str_image))
+    fh.close()
+    cnt+=1
+
+    return json.dumps({"upload": "image"})
+
--- test_send_restful.py
+++ test_send_restful.py
@@ -1,8 +1,29 @@
 # importing the requests library
-import requests
-  
+import requests, json, base64
+
 # defining the api-endpoint 
-API_ENDPOINT = "http://127.0.0.1:8000/upload"
+api_url = "http://127.0.0.1:8000/upload"
+image_file = '/home/cmit/dev_ws/ham_image/rgb_0.png'
+
+headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+data = open(image_file, 'rb').read()
+
+with open(image_file, "rb") as f:
+    im_bytes = f.read()        
+im_b64 = base64.b64encode(im_bytes).decode("utf8")
+#print(im_b64)
+
+payload = json.dumps({"image": im_b64})
+
+response = requests.post(api_url, data=payload, headers=headers)
+try:
+    data = response.json()     
+    print(data)                
+except requests.exceptions.RequestException:
+    print(response.text)
+
+exit()
 
 # your API key here
 API_KEY = "XXXXXXXXXXXXXXXXX"
@@ -22,7 +43,7 @@ data = {'api_dev_key':API_KEY,
         'api_paste_format':'python'}
 
 # sending post request and saving response as response object
-res = requests.post(url = API_ENDPOINT, data = data)
+res = requests.post(url=api_url, data=payload, headers=headers)
 
 # extracting response text 
 print(type(res))
